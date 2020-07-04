20f66f6dfbcacbbc5739a78a3e1f80416ce5bcfe
Tim Savage <tim.savage@gmail.com>
2020-07-04 09:10:14 -0500

    add javadoc; mark method parameters final

--- LodeStar/src/main/java/com/winterhaven_mc/lodestar/messages/Message.java
+++ LodeStar/src/main/java/com/winterhaven_mc/lodestar/messages/Message.java
@@ -6,11 +6,26 @@ import org.bukkit.command.CommandSender;
 
 public class Message extends AbstractMessage<MessageId,Macro> {
 
-	private Message(CommandSender recipient, MessageId messageId) {
+
+	/**
+	 * Private class constructor; calls inherited super constructor
+	 *
+	 * @param recipient the message recipient
+	 * @param messageId the enum entry representing the message to be displayed
+	 */
+	private Message(final CommandSender recipient, final MessageId messageId) {
 		super(recipient, messageId);
 	}
 
-	public static Message create(CommandSender recipient, MessageId messageId) {
+
+	/**
+	 * Static class constructor
+	 *
+	 * @param recipient the message recipient
+	 * @param messageId the enum entry representing the message to be displayed
+	 * @return new instance of Message created with private constructor
+	 */
+	public static Message create(final CommandSender recipient, final MessageId messageId) {
 		return new Message(recipient, messageId);
 	}
 
