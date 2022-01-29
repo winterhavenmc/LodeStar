04b8b9d1db84e8925dcde0feebff97ca8001305c
Tim Savage <tim.savage@gmail.com>
2022-01-29 01:01:38 -0600

    remove setters

--- src/main/java/com/winterhavenmc/lodestar/commands/Subcommand.java
+++ src/main/java/com/winterhavenmc/lodestar/commands/Subcommand.java
@@ -17,32 +17,19 @@ interface Subcommand {
 
 	String getName();
 
-	void setName(final String name);
-
 	Collection<String> getAliases();
-
-	@SuppressWarnings("unused")
-	void setAliases(final Collection<String> aliases);
 
 	void addAlias(final String alias);
 
 	@SuppressWarnings("unused")
 	String getUsage();
 
-	void setUsage(final String usageString);
-
 	void displayUsage(final CommandSender sender);
 
 	MessageId getDescription();
 
-	void setDescription(final MessageId messageId);
-
 	int getMinArgs();
 
-	void setMinArgs(final int maxArgs);
-
 	int getMaxArgs();
-
-	void setMaxArgs(final int maxArgs);
 
 }
