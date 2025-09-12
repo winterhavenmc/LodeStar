3e57bbf59c3039ada20091cae2536e3c9760b83e
Tim Savage <tim.savage@gmail.com>
2025-09-12 00:33:51 -0500

    move static factory methods to subclasses; remove obsolete enum

--- models/src/main/java/com/winterhavenmc/lodestar/models/destination/Destination.java
+++ models/src/main/java/com/winterhavenmc/lodestar/models/destination/Destination.java
@@ -17,94 +17,16 @@
 
 package com.winterhavenmc.lodestar.models.destination;
 
-import com.winterhavenmc.lodestar.models.location.*;
-import org.bukkit.Location;
-
-import java.util.Objects;
-import java.util.UUID;
-
+import com.winterhavenmc.library.messagebuilder.pipeline.adapters.displayname.DisplayNameable;
 
 /**
- * Represents an immutable location and a corresponding display name. A {@link Location} is decomposed into validated,
+ * Represents an immutable location and a corresponding display name. A location is decomposed into validated,
  * immutable fields, and reconstituted into a Bukkit {@code Location} on access. Static factory methods are provided to
  * create new instances when reading from a datastore, or creating an instance from an existing Bukkit {@code Location}
  * and a corresponding {@code String} display name.
  */
-public sealed interface Destination permits ValidDestination, InvalidDestination
+
+public sealed interface Destination extends DisplayNameable permits ValidDestination, InvalidDestination
 {
-	/**
-	 * Enumerates the types of valid destinations.
-	 */
-	enum Type { HOME, SPAWN, STORED }
-
-
-	/**
-	 * Returns a new instance of a {@code Destination} of the appropriate type from the given parameters.
-	 *
-	 * @param type        the type of destination
-	 * @param displayName the display name of the destination
-	 * @param location    the location of the destination
-	 * @return a {@link ValidDestination}, or an {@link InvalidDestination} if a valid destination could not be creaated.
-	 */
-	static Destination of(final Type type,
-	                      final String displayName,
-	                      final Location location)
-	{
-		return Destination.of(type, displayName, Objects.requireNonNull(location.getWorld()).getName(),
-				location.getWorld().getUID(), location.getX(), location.getY(), location.getZ(),
-				location.getYaw(), location.getPitch());
-	}
-
-
-	/**
-	 * Returns a new instance of a {@code Destination} of the appropriate type from the given parameters.
-	 *
-	 * @param type        the type of destination
-	 * @param displayName the display name of the destination
-	 * @param location    the location of the destination
-	 * @return a {@link ValidDestination}, or an {@link InvalidDestination} if a valid destination could not be creaated.
-	 */
-	static Destination of(final Type type,
-	                      final String displayName,
-	                      final ValidLocation location)
-	{
-		return Destination.of(type, displayName, location.world().getName(),
-				location.world().getUID(), location.x(), location.y(), location.z(),
-				location.yaw(), location.pitch());
-	}
-
-
-	/**
-	 * Create a new instance of a {@code Destination} from values retrieved from the datastore.
-	 *
-	 * @return a {@link ValidDestination}, or an {@link InvalidDestination} if a valid destination could not be creaated.
-	 */
-	static Destination of(final Type type,
-	                      final String displayName,
-	                      final String worldName,
-	                      final UUID worldUid,
-	                      final double x,
-	                      final double y,
-	                      final double z,
-	                      final float yaw,
-	                      final float pitch)
-	{
-		if (displayName == null) return new InvalidDestination("∅", "The display name was null.");
-		if (displayName.isBlank()) return new InvalidDestination("⬚", "The display name was blank.");
-		if (type == null) return new InvalidDestination(displayName, "The type was null.");
-
-		return switch (ImmutableLocation.of(worldName, worldUid, x, y, z, yaw, pitch))
-		{
-			case InvalidLocation ignored -> new InvalidDestination(displayName, "The location was invalid.");
-			case NoWorldLocation ignored -> new InvalidDestination(displayName, "The location had an invalid world.");
-			case UnloadedWorldLocation ignored -> new InvalidDestination(displayName, "The location world is unloaded.");
-			case ValidLocation validLocation -> switch (type)
-					{
-						case HOME -> new HomeDestination(displayName, validLocation);
-						case SPAWN -> new SpawnDestination(displayName);
-						case STORED -> new StoredDestination(displayName, validLocation);
-					};
-		};
-	}
-
+	String getDisplayName();
 }
